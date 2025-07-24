32a843e623b6ffad82b5597dbc8171f32258df52
anisha1045 <144181985+anisha1045@users.noreply.github.com>
2025-07-24 15:16:30 -0500

    Worked on saving to tables.

---
+++ app/api/add-reminder/route.js
@@ -0,0 +1,15 @@
+import { supabase } from '@/utils/supabase'  // adjust path if needed
+
+export async function POST(req) {
+  const body = await req.json()
+  console.log("POSTING TO SUPABASE");
+  const { data, error } = await supabase
+    .from('Reminder')
+    .insert([{ ...body }])
+
+    if (error) {
+      console.error("Supabase insert error:", error)
+      return Response.json({ error }, { status: 500 })
+    }
+  return Response.json({ success: true, data })
+}
--- app/api/extract/route.js
+++
@@ -1,5 +0,0 @@
-export async function POST(request) {
-    const body = await request.json()
-    console.log('EXTRACTED');
-    return Response.json({ message: 'Extracted!', received: body })
-  }
